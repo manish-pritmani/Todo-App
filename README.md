7e3c9c8d7738b2ebba7c5fa198aa782f23cd4501
Manish Pritmani <manish.pritmani06@gmail.com>
2021-01-07 19:57:12 +0530

    Completed

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-To-do Application
+# Simple-Todo-Application
+In this repository, I have developed a Todo-Application.
 
-1. Package - uuid
-2. Resources from - font awesome
-3. Hooks used - useState, useEffect,useRef
+![OverView](https://github.com/manish-pritmani/Todo-App/screenshots/main.png?raw=true)
+
+> If you found this project helpful, or you learned something from the source code and want to thank me, consider buying me a cup of :coffee:
+
+> - [BuyMeACoffee](https://www.buymeacoffee.com/manishpritmani)
+> - [PayPal](https://www.paypal.me/manishpritmani/)
+
+### Show some :heart: and star the repo.
--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 
 function App() {
   const firstLoad = useRef(true);
-//Initial states won't allow us to change the input value
   const [inputValue,setInputValue] = useState('');
   const [allTodos,setAllTodos] = useState([]);
 
@@ -12,10 +11,7 @@ function App() {
     e.preventDefault();
     if(inputValue.trim() === '')
         return;
-//    console.log('test');
-//Spread operator ...  will fetch all previous todos first
     setAllTodos([...allTodos, {
-//    We can have as many object values as we want.
     text: inputValue,
     id: uuidv4(),
     }]);
@@ -24,25 +20,18 @@ function App() {
   }
 
   const removeTodo = (id) =>{
-//    allTodos.delete((id)=> todo.id);
     setAllTodos(allTodos.filter((todo) => todo.id !== id));
   };
 
-//  use effect will be fired each time the array got new event
   useEffect(() =>{
       if (firstLoad.current){
-//        console.log('true');
         firstLoad.current = false;
       } else {
-//    for first render we don't need the local storage, but after first the local storage comes into play
         localStorage.setItem("todoList", JSON.stringify([...allTodos]));
-//        console.log('false');
       }
     },[allTodos]);
 
-//  This fires everytime as the array is left blank
   useEffect(() => {
-//  here null condition is there because if we don't have localStorage instance then we can't get that and will throw the error in console.
   if (localStorage.getItem("todoList") !== null){
         const newTodos = localStorage.getItem("todoList");
         setAllTodos(JSON.parse([...allTodos, newTodos]));
@@ -55,10 +44,6 @@ function App() {
             <form onSubmit={addTodo}>
             <input autoFocus type="text" placeholder="Enter Todo Task..."
             value={inputValue}
-//          Whatever we type get stored in the input value
-//          Our Todos are in array, so we use array mapping, every time we have array we need to use map
-//          If allTodos map has parenthesis then it processes whats inside it, unlike the curly braces we don't need array braces.
-//          In React its className in stead of class.
             onChange={(e) => setInputValue(e.target.value) }/>
             <button type="submit">Add Todo</button>
             </form>
